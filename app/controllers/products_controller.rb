ac62d9c91ee64642462b99ef8419ce682a96008b
Piotr Czarny <piotrczarny95@wp.pl>
2017-05-04 10:16:22 +0200

    change

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,6 +26,8 @@ def correct_user
 
   def show
  
+  @product = Product.find(params[:id])
+  @reviews = @product.reviews
   @table = @reviews.map {|review| review.rating}
   @average = @table.inject(0.0) { |sum, el| sum + el }/ @table.size
   product.average_rating = '%.1f'% @average
